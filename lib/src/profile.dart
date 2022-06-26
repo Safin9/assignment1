614fff5c1c1ef503d22eec96753ec798d1a6c711
safin saber <safinsabir@gmail.com>
2022-06-26 18:03:05 +0300

    used stack instead of appbar

--- lib/src/profile.dart
+++ lib/src/profile.dart
@@ -7,44 +7,55 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(student.firstName),
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 300,
-              width: 400,
-              child: Image.network(
-                student.image!,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    student.firstName,
-                    style: const TextStyle(fontSize: 25),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 62.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(children: [
+                SizedBox(
+                  height: 300,
+                  width: 400,
+                  child: Image.network(
+                    student.image!,
+                    fit: BoxFit.cover,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                ],
+                ),
+                Positioned(
+                    top: 20,
+                    left: 10,
+                    child: FloatingActionButton(
+                      elevation: 6,
+                      backgroundColor: Colors.transparent,
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Icon(Icons.arrow_back),
+                    )),
+              ]),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      student.firstName,
+                      style: const TextStyle(fontSize: 25),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Wrap(
-                children: [Text(student.description ?? 'no description')],
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Wrap(
+                  children: [Text(student.description ?? 'no description')],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
